d72175e6a986b2fb89474601deb35bb88ede748d
Shweta M <shwetamudbikar2003@gmail.com>
2026-07-14 00:22:31 +0530

    Implemented Django authentication and user registration

--- Backend/accounts/views.py
+++ Backend/accounts/views.py
@@ -1,4 +1,25 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def home(request):
-    return HttpResponse("Welcome to CareerPilot AI 🚀")
+    return render(request, "home.html")
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists!")
+        else:
+            User.objects.create_user(
+                username=username,
+                email=email,
+                password=password
+            )
+            messages.success(request, "Registration Successful!")
+            return redirect("register")
+
+    return render(request, "register.html")
--- Backend/careerpilot/settings.py
+++ Backend/careerpilot/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'careerpilot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -72,10 +74,14 @@ WSGI_APPLICATION = 'careerpilot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
+DATABASES ={
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'careerpilotai',
+        'USER': 'root',
+        'PASSWORD': 'shweta@123456',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
